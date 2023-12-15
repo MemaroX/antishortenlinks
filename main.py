908a8efb81bf43c1c03f3175f997b3e2cf6a3efd
Maher Gomaa Ismaeel Sayed <116512721+MemaroX@users.noreply.github.com>
2023-12-15 21:00:57 +0200

    Create main.py
    This is the program i made

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+import time
+import webbrowser
+
+def is_safe(url):
+    api_key = 'APIKEY'
+    url_id = requests.get('https://www.virustotal.com/vtapi/v2/url/report',
+                          params={'apikey': api_key, 'resource':url})
+    report = url_id.json()
+    if 'positives' in report and report['positives'] > 0:
+        return False
+    else:
+        return True
+
+def print_redirects(url):
+    final_url = url
+    with requests.Session() as session:
+        try:
+            response = session.get(url, allow_redirects=False)
+            print(response.url)
+            print(f'Is URL safe? {is_safe(response.url)}')
+            while 'location' in response.headers:
+                time.sleep(5)  # Wait for 5 seconds
+                url = response.headers['location']
+                response = session.get(url, allow_redirects=False)
+                print(response.url)
+                print(f'Is URL safe? {is_safe(response.url)}')
+                final_url = response.url
+        except requests.exceptions.RequestException as e:
+            print(f'An error occurred: {e}')
+    return final_url  # Return the final URL
+
+link = input('Enter the link you want: ')
+result = print_redirects(link)  # Assign the final URL to 'result'
+print('Do you want to go to the result immediately?')
+answer = input('Yes or No?\n')
+if answer == 'Y' or answer == 'Yes' or answer == 'yes' or answer == 'y':
+    webbrowser.open(result)  # Open the final URL
+    exit()
+else:
+    exit()
